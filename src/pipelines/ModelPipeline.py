6008828b1748e8c3fdf42d743f935d7f13f4cb8d
Yashuu05 <chillalyash2005@gmail.com>
2026-03-29 11:04:25 +0530

    created pipeline

---
+++ src/pipelines/ModelPipeline.py
@@ -0,0 +1,14 @@
+from src.pipelines.Pipeline import create_pipeline
+from sklearn.pipeline import Pipeline
+import os
+
+def create_model_pipeline(model):
+    df = os.path.join(os.path.dirname(__file__), '..', '..', 'data', 'X_train.csv')
+    preprocessor = create_pipeline(file_path=df)
+
+    model_pipline = Pipeline(steps=[
+        ('preprocessor', preprocessor),
+        ('model', model)
+    ])
+
+    return model_pipline
